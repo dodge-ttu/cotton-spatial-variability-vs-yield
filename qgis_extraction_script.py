c50096528698280a55fc1aa029b33c30c9263579
will <dodge.ttu@gmail.com>
2019-01-03 10:44:49 -0600

    update

--- qgis_extraction_script.py
+++ qgis_extraction_script.py
@@ -3,96 +3,94 @@ import sys
 from datetime import datetime
 from qgis.core import QgsProject
 from qgis.core import QgsApplication
-from qgis.core import QgsVectorFileWriter
 import processing
 from processing.core.Processing import Processing
 
-# Append path to QGIS.
-sys.path.append("/home/will/cotton spatial variability vs yield analysis/cott_spat_interp/lib/python3/dist-packages")
-
-# Get date to tag output.
-raw_time = datetime.now()
-formatted_time = datetime.strftime(raw_time, format="%Y-%m-%d %H:%M:%S")
-
-# Define path to output directory.
-output_dir = "/home/will/cotton spatial variability vs yield analysis/2018-rain-matrix-p7-p6-extractions-and-data/2018_p7_p6_extractions/p7"
-#output_dir = "/home/will/cotton spatial variability vs yield analysis/cotton-spatial-variability-vs-yield/2018-rain-matrix-p7-p6-extractions-and-data/2018_p7_p6_extractions/p7"
-
-# Define input layer.
-input_layer = "2018-06-21_75_75_20_rainMatrix_odm_orthophoto_modified"
-
-# Create a reference to the QGIS application.
-qgs = QgsApplication([], False)
-
-# Load providers.
-qgs.initQgis()
-
-# Create a project instance.
-project = QgsProject.instance()
-
-# Load a project.
-project.read('/home/will/MAHAN MAP 2018/MAHAN MAP 2018.qgs')
-
-# Initialize processing.
-Processing.initialize()
-
-# Create bridge between loaded project and canvas
-bridge = project.layerTreeRegistryBridge()
-
-# Return the layer tree and isolate the group of interest to programmatically extract the individual
-my_layer_tree = QgsProject.instance().layerTreeRoot()
-my_group = my_layer_tree.findGroup("spatial_analysis_p7_aoms")
-#my_group = my_layer_tree.findGroup("spatial_analysis_p7_aoms")
-
-# Generate a list of items in the group of interest.
-layer_list = my_group.children()
-
-# Details
-planting = 'p7'
-what = 'aoms'
-
-# Create an out directory.
-directory_path = os.path.join(output_dir, "{0}-{1}-extracted".format(planting, what))
-if not os.path.exists(directory_path):
-    os.makedirs(directory_path)
-
 
 # Function to iteratively extract virtual sample spaces.
-def make_samples(layer_list=None, output_dir=None, input_layer_name=None, **kwargs):
-    
+def make_samples(layer_list=None, output_dir=None, input_layer_name=None):
     for i in layer_list:
-        
         parameters = {
-        'ALPHA_BAND': False,
-        'CROP_TO_CUTLINE': True,
-        'DATA_TYPE': 0,
-        'INPUT': '{0}'.format(input_layer_name),
-        'KEEP_RESOLUTION': True,
-        'MASK': '{0}'.format(i.name()),
-        'NODATA': None,
-        'OPTIONS': '',
-        'OUTPUT': os.path.join(output_dir, "{0}.tif".format(i.name())),
-       }
+            'ALPHA_BAND': False,
+            'CROP_TO_CUTLINE': True,
+            'DATA_TYPE': 0,
+            'INPUT': '{0}'.format(input_layer_name),
+            'KEEP_RESOLUTION': True,
+            'MASK': '{0}'.format(i.name()),
+            'NODATA': None,
+            'OPTIONS': '',
+            'OUTPUT': os.path.join(output_dir, "{0}.tif".format(i.name())),
+        }
 
         processing.run('gdal:cliprasterbymasklayer', parameters)
 
 
-# Process the eraly sample spaces.
-params = {'output_dir':directory_path,
-          'layer_list': layer_list,
-          'input_layer_name':input_layer}
+if __name__ == '__main__':
 
-make_samples(**params)
+    # Append path to QGIS.
+    sys.path.append("/home/will/cotton spatial variability vs yield analysis/"
+                    "cott_spat_interp/lib/python3/dist-packages")
 
-# Write a meta-data file with the details of this extraction for future referecne.
-with open(os.path.join(output_dir, "sample_meta_data.txt"), "w") as tester:
-    tester.write("""Sample Layer ID: {0}\n
-                    Number of Samples: {1}\n
-                    Samples Generated On: {2}\n
-                    """.format(input_layer, len(layer_list), formatted_time))
+    # Get date to tag output.
+    raw_time = datetime.now()
+    formatted_time = datetime.strftime(raw_time, "%Y-%m-%d %H:%M:%S")
 
-tester.close()
+    # Define path to output directory.
+    an_output_dir = "/home/will/cotton spatial variability vs yield analysis/" \
+        "2018-rain-matrix-p7-p6-extractions-and-data/2018_p7_p6_extractions/p6"
 
-# Close project.
-qgs.exitQgis()
+    # Define input layer.
+    input_layer = "2018-06-21_75_75_20_rainMatrix_odm_orthophoto_modified"
 
+    # Create a reference to the QGIS application.
+    qgs = QgsApplication([], False)
+
+    # Load providers.
+    qgs.initQgis()
+
+    # Create a project instance.
+    project = QgsProject.instance()
+
+    # Load a project.
+    project.read('/home/will/MAHAN MAP 2018/MAHAN MAP 2018.qgs')
+
+    # Initialize processing.
+    Processing.initialize()
+
+    # Create bridge between loaded project and canvas
+    bridge = project.layerTreeRegistryBridge()
+
+    # Return the layer tree and isolate the group of interest to programmatically extract the individual
+    my_layer_tree = QgsProject.instance().layerTreeRoot()
+    my_group = my_layer_tree.findGroup("spatial_analysis_p6_aoms")
+
+    # Generate a list of items in the group of interest.
+    a_layer_list = my_group.children()
+
+    # Details
+    planting = 'p6'
+    what = 'aoms'
+
+    # Create an out directory.
+    directory_path = os.path.join(an_output_dir, "{0}-{1}-extracted".format(planting, what))
+    if not os.path.exists(directory_path):
+        os.makedirs(directory_path)
+
+    # Process the eraly sample spaces.
+    params = {'output_dir': directory_path,
+              'layer_list': a_layer_list,
+              'input_layer_name': input_layer}
+
+    make_samples(**params)
+
+    # Write a meta-data file with the details of this extraction for future referecne.
+    with open(os.path.join(directory_path, "sample_meta_data.txt"), "w") as tester:
+        tester.write("""Sample Layer ID: {0}\n
+                        Number of Samples: {1}\n
+                        Samples Generated On: {2}\n
+                        """.format(input_layer, len(a_layer_list), formatted_time))
+
+    tester.close()
+
+    # Close project.
+    qgs.exitQgis()
--- save-point-layer-as-csv.py
+++ save-point-layer-as-csv.py
@@ -8,60 +8,16 @@ from qgis.core import QgsProject
 from qgis.core import QgsApplication
 from qgis.core import QgsVectorFileWriter
 from qgis.core import QgsCoordinateReferenceSystem
-from qgis.core import QgsVectorLayer
-import processing
-from processing.core.Processing import Processing
 
-
-# Append path to QGIS.
-sys.path.append("/home/will/cotton spatial variability vs yield analysis/cott_spat_interp/lib/python3/dist-packages")
-
-# Get date to tag output.
-raw_time = datetime.now()
-formatted_time = datetime.strftime(raw_time, format="%Y-%m-%d %H:%M:%S")
-
-# Define path to output directory.
-output_dir = "/home/will/cotton spatial variability vs yield analysis/2018-rain-matrix-p7-p6-extractions-and-data/2018_p7_p6_extractions/p6"
-
-# Create a reference to the QGIS application.
-qgs = QgsApplication([], False)
-
-# Load providers.
-qgs.initQgis()
-
-# Create a project instance.
-project = QgsProject.instance()
-
-# Load a project.
-project.read('/home/will/MAHAN MAP 2018/MAHAN MAP 2018.qgs')
-
-# Initialize processing.
-Processing.initialize()
-
-# Get map layers.
-map_layers = project.mapLayers()
-
-# Details.
-planting = 'p6'
-what = 'points'
-
-a_layer_of_points = [v for k,v in map_layers.items() if planting in k and what in k]
-
-# Create a directory to hold partially processed copies of shapefile layers as
-# processing runs are called on the layers.
-
-directory_path = os.path.join(output_dir, "{0}-{1}-shapefiles".format(planting, what))
-if not os.path.exists(directory_path):
-    os.makedirs(directory_path)
 
 # Change layer CRS for a list of layers:
-def change_projections(layer_list, **kwargs):
+def change_projections(layer_list=None, output_dir=None):
 
-    for layer in a_layer_of_points:
+    for layer in layer_list:
 
         parameters = {
             'layer': layer,
-            'fileName': os.path.join(directory_path, "{0}_process".format(layer.name())),
+            'fileName': os.path.join(output_dir, "{0}_process".format(layer.name())),
             'fileEncoding': "utf-8",
             'destCRS': QgsCoordinateReferenceSystem('EPSG:4326'),
             'driverName': 'CSV',
@@ -74,26 +30,63 @@ def change_projections(layer_list, **kwargs):
         # Use ** to unpack the dictionary as arguments for the function
         QgsVectorFileWriter.writeAsVectorFormat(**parameters)
 
-# Create a directory to hold partially processed copies of shapefile layers as
-# processing runs are called on the layers.
 
-directory_path = os.path.join(output_dir, "{0}-{1}-shapefiles".format(planting, what))
-if not os.path.exists(directory_path):
-    os.makedirs(directory_path)
+if __name__ == '__main__':
 
-params = {'output_path':directory_path}
+    # Append path to QGIS.
+    sys.path.append("/home/will/cotton spatial variability vs yield analysis/"
+                    "cott_spat_interp/lib/python3/dist-packages")
 
-change_projections(a_layer_of_points)
+    # Get date to tag output.
+    raw_time = datetime.now()
+    formatted_time = datetime.strftime(raw_time, "%Y-%m-%d %H:%M:%S")
 
+    # Define path to output directory.
+    output_dir = "/home/will/cotton spatial variability vs yield analysis/" \
+                 "2018-rain-matrix-p7-p6-extractions-and-data/2018_p7_p6_extractions/p6"
 
-# Write a meta-data file with the details of this extraction for future referecne.
-with open(os.path.join(directory_path, "sample_meta_data.txt"), "w") as tester:
-    tester.write("""planting: {0}\n
-                    what was extracted: {1}\n
-                    Samples Generated On: {2}\n
-                    """.format(planting, what, formatted_time))
+    # Create a reference to the QGIS application.
+    qgs = QgsApplication([], False)
 
-tester.close()
+    # Load providers.
+    qgs.initQgis()
 
-# Close project.
-qgs.exitQgis()
+    # Create a project instance.
+    project = QgsProject.instance()
+
+    # Load a project.
+    project.read('/home/will/MAHAN MAP 2018/MAHAN MAP 2018.qgs')
+
+    # Get map layers.
+    map_layers = project.mapLayers()
+
+    # Details.
+    planting = 'p6'
+    what = 'points'
+
+    a_layer_of_points = [v for k, v in map_layers.items() if planting in k and what in k]
+
+    # Create a directory to hold partially processed copies of shapefile layers as
+    # processing runs are called on the layers.
+
+    directory_path = os.path.join(output_dir, "{0}-{1}-csv-data".format(planting, what))
+    if not os.path.exists(directory_path):
+        os.makedirs(directory_path)
+
+    # Run function
+    params = {'output_dir': directory_path,
+              'layer_list': a_layer_of_points}
+
+    change_projections(**params)
+
+    # Write a meta-data file with the details of this extraction for future referecne.
+    with open(os.path.join(directory_path, "sample_meta_data.txt"), "w") as tester:
+        tester.write("""planting: {0}\n
+                        what was extracted: {1}\n
+                        Samples Generated On: {2}\n
+                        """.format(planting, what, formatted_time))
+
+    tester.close()
+
+    # Close project.
+    qgs.exitQgis()
